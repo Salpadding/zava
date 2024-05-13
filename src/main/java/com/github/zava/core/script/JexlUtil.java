69a145f7fe6712306160ac0a13c5dc7e6b7e2311
a <a>
2024-05-13 18:41:28 +0800

    complete jexl util

--- src/main/java/com/github/zava/core/script/JexlUtil.java
+++ src/main/java/com/github/zava/core/script/JexlUtil.java
@@ -4,10 +4,9 @@ import lombok.SneakyThrows;
 import org.apache.commons.jexl3.*;
 import org.apache.commons.jexl3.introspection.JexlPermissions;
 
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.ConcurrentSkipListMap;
 
-// 执行 jexl 表达式
 public class JexlUtil {
     public static final Map<String, JexlScript> CACHE = new ConcurrentSkipListMap<>();
     public static final JexlEngine ENGINE = new JexlBuilder()
@@ -15,8 +14,48 @@ public class JexlUtil {
         .features(new JexlFeatures())
         .create();
 
-    @SneakyThrows
+
+    public static List<Object> evalAll(Map<String, Object> payload, String[] expr) {
+        return evalAll(payload, expr, false);
+    }
+
+    public static List<Object> evalAll(Map<String, Object> payload, Collection<? extends String> exprs) {
+        return evalAll(payload, exprs, false);
+    }
+
+    public static List<Object> evalAll(Map<String, Object> payload, Collection<? extends String> exprs, boolean cache) {
+        if (exprs == null) return Collections.emptyList();
+        List<Object> ret = new ArrayList<>();
+        for (String expr : exprs) {
+            ret.add(eval(payload, expr, cache));
+        }
+        return ret;
+    }
+
+    public static List<Object> evalAll(Map<String, Object> payload, String[] expr, boolean cache) {
+        if (expr == null) return Collections.emptyList();
+        return evalAll(payload, Arrays.asList(expr), cache);
+    }
+
     public static Object eval(Map<String, Object> payload, String expr) {
+        return eval(payload, expr, false);
+    }
+
+    public static List<Object> evalAll(Collection<? extends Map<String, Object>> payloads, String expr, boolean cache) {
+        if (payloads == null || payloads.isEmpty() || expr == null) return Collections.emptyList();
+        List<Object> ret = new ArrayList<>();
+        for (Map<String, Object> payload : payloads) {
+            ret.add(eval(payload, expr, cache));
+        }
+        return ret;
+    }
+
+    @SneakyThrows
+    public static Object eval(Map<String, Object> payload, String expr, boolean cache) {
+        if (expr == null) return null;
+        if (!cache)
+            return ENGINE.createScript(expr).execute(createContext(payload));
+
         JexlScript script = CACHE.get(expr);
         boolean put = script == null;
         if (script == null)
@@ -29,6 +68,7 @@ public class JexlUtil {
 
     public static JexlContext createContext(Map<String, Object> msg) {
         MapContext ret = new MapContext();
+        if (msg == null) return ret;
         for (Map.Entry<String, Object> entry : msg.entrySet()) {
             ret.set(entry.getKey(), entry.getValue());
         }
